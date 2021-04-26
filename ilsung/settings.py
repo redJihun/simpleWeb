ad2509add458506bd874cf991ee3a0996a8e8e9d
Jihun Hong <hjihun2315@gmail.com>
2021-04-26 17:07:11 +0900

    feat: django startapp
    intro, business, portfolio 추가 + url 연결 + settings.py installed_apps 연결

--- ilsung/settings.py
+++ ilsung/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'intro.apps.IntroConfig',
+    'business.apps.BusinessConfig',
+    'portfolio.apps.PortfolioConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
