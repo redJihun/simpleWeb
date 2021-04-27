9ef8053157ae367bc065ad3413e81ae56ea15fe7
Jihun Hong <hjihun2315@gmail.com>
2021-04-27 11:20:02 +0900

    feat: homepage
    homeIndex-url 매핑, homeIndex-view 추가, homeIndex-template 연결

---
+++ home/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'home/index.html'
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
