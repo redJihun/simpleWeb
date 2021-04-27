69379605a9ac8494b46914dbea6c871da573438f
Jihun Hong <hjihun2315@gmail.com>
2021-04-27 12:29:36 +0900

    feat: 링크연결 및 스태틱 구성
    링크수정, django static 로직 구성 및 연결

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='homeIndex'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
