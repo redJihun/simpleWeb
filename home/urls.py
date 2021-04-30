b9682f0352b7149ad76bcec319458e8f3554908f
Jihun Hong <hjihun2315@gmail.com>
2021-04-30 16:13:51 +0900

    style: 테마색상변경, intro 페이지 리뉴얼, business 페이지 리뉴얼

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 from .apps import HomeConfig as config
 
 app_name = config.name
 urlpatterns = [
     path('', views.IndexView.as_view(), name='homeIndex'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
