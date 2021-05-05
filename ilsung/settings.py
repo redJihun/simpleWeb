ad46595297daf3fe3d07104b2ba9404ee8a0e929
Jihun Hong <hjihun2315@gmail.com>
2021-05-05 20:06:37 +0900

    Update settings.py

--- ilsung/settings.py
+++ ilsung/settings.py
@@ -164,7 +164,13 @@ CKEDITOR_UPLOAD_PATH = 'uploads/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
