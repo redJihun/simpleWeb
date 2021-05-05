6a9f3700beee3963a975514f853fc4a7306ae333
Jihun Hong <hjihun2315@gmail.com>
2021-05-05 19:33:52 +0900

    refac: 보안 문제 코드 변경

--- ilsung/settings.py
+++ ilsung/settings.py
@@ -38,7 +38,8 @@ def get_secret(setting, secrets=secrets):
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
