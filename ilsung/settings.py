d6f367d607ffbffef30bae9f4fcf371d109a3031
Jihun Hong <hjihun2315@gmail.com>
2021-05-05 20:50:05 +0900

    Update settings.py

--- ilsung/settings.py
+++ ilsung/settings.py
@@ -179,3 +179,6 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+# Activate Django-Heroku.
+import django_heroku
+django_heroku.settings(locals())
