e96604e7c14055f2d9d3e7d0cfd2c1d25ea6524f
Jihun Hong <hjihun2315@gmail.com>
2021-05-05 20:31:19 +0900

    Update settings.py

--- ilsung/settings.py
+++ ilsung/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os, json
 from django.core.exceptions import ImproperlyConfigured
-from boto.s3.connection import S3Connection
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
